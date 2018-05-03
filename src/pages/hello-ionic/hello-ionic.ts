08dd2aaa4c71600b31cf7411886993ba6a966ddd
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2018-05-02 22:00:52 -0500

    Add show alert message.

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  constructor(private alert: AlertController) {
 
   }
+
+  showAlert() {
+    console.log('being clicked');
+    this.alert.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    }).present();
+  }
+
 }
